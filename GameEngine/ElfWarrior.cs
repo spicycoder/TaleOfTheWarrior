6b9a3832d955f1509815c001a54962080df5dc89
Naveen <spk2naveen@hotmail.com>
2018-10-18 21:27:52 +0530

    Finished solution

---
+++ GameEngine/ElfWarrior.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace GameEngine
+{
+    public class ElfWarrior
+    {
+        public int Health { get; set; }
+
+        public ElfWarrior()
+        {
+            Health = 30;
+        }
+
+        public void Eat(FoodTypes food)
+        {
+            Health = Math.Min(Health + Food.CalariesIndex[food], 100);
+        }
+
+        public void Hit()
+        {
+            Health = Math.Max(Health - 10, 0);
+        }
+    }
+}
